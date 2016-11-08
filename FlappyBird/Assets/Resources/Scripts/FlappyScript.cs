387f8fd61aeab24b7112daa41c888a781b9dae59
bysen32 <bysen32@gmail.com>
2016-11-09 01:33:12 +0800

    add scoreManager + UI

--- FlappyBird/Assets/Resources/Scripts/FlappyScript.cs
+++ FlappyBird/Assets/Resources/Scripts/FlappyScript.cs
@@ -3,20 +3,34 @@ using System.Collections;
 
 public class FlappyScript : MonoBehaviour {
 
+    public GameObject IntroGUI, DeathGUI;
+    public Collider2D restartButtonGameCollider;
+
     public float XSpeed = 1;
     public float VelocityPreJump = 3;
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         if (Input.GetKeyDown(KeyCode.Escape))
             Application.Quit();
 
         if (GameStateManager.GameState == GameState.Intro)
         {
+            MoveBirdOnXAxis();
+            if (WasTouchedOrClicked())
+            {
+                BoostOnYAxis();
+                GameStateManager.GameState = GameState.Playing;
+                IntroGUI.SetActive(false);
+                ScoreManagerScript.Score = 0;
+            }
 
         }
         else if (GameStateManager.GameState == GameState.Playing)
@@ -30,13 +44,32 @@ public class FlappyScript : MonoBehaviour {
         }
         else if (GameStateManager.GameState == GameState.Dead)
         {
+            Vector2 contactPoint = Vector2.zero;
+            if (Input.touchCount > 0)
+                contactPoint = Input.touches[0].position;
+            if (Input.GetMouseButton(0))
+                contactPoint = Input.mousePosition;
 
+            if (restartButtonGameCollider == Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(contactPoint)))
+            {
+                GameStateManager.GameState = GameState.Intro;
+                Application.LoadLevel(Application.loadedLevelName);
+            }
         }
 	}
 
-    void MoveBirdOnXAxis()
+    void FixedUpdate()
     {
-        transform.position += new Vector3(Time.deltaTime * XSpeed, 0, 0);
+        if (GameStateManager.GameState == GameState.Intro)
+        {
+            if (GetComponent<Rigidbody2D>().velocity.y < -1)
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, GetComponent<Rigidbody2D>().mass * 5500 * Time.deltaTime));
+        }
+        else if (GameStateManager.GameState == GameState.Playing || GameStateManager.GameState == GameState.Dead)
+        {
+            FixFlappyRotation();
+        }
+
     }
 
     bool WasTouchedOrClicked()
@@ -47,8 +80,18 @@ public class FlappyScript : MonoBehaviour {
             return false;
     }
 
+    void MoveBirdOnXAxis()
+    {
+        transform.position += new Vector3(Time.deltaTime * XSpeed, 0, 0);
+    }
+
     void BoostOnYAxis()
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(0, VelocityPreJump);
+    }
+
+    private void FixFlappyRotation()
+    {
+
     }
 }
---
+++ FlappyBird/Assets/Resources/Scripts/ScoreManagerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ScoreManagerScript : MonoBehaviour {
+
+    public static int Score { get; set; }
+
+    int previousScore = -1;
+    public Sprite[] numberSprites;
+    public SpriteRenderer Units, Tens, Hundreds;
+
+    void Start()
+    {
+        Tens.gameObject.SetActive(false);
+        Hundreds.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (previousScore != Score)
+        {
+            if (Score < 10)
+            {
+                Units.sprite = numberSprites[Score];
+            }
+            else if (Score >= 10 && Score < 100)
+            {
+                Tens.gameObject.SetActive(true);
+                Tens.sprite = numberSprites[Score / 10];
+                Units.sprite = numberSprites[Score % 10];
+            }
+            else if(Score >= 100)
+            {
+                Hundreds.gameObject.SetActive(true);
+                Hundreds.sprite = numberSprites[Score / 100];
+                int rest = Score % 100;
+                Tens.sprite = numberSprites[rest / 10];
+                Units.sprite = numberSprites[rest % 100];
+            }
+        }
+    }
+}
