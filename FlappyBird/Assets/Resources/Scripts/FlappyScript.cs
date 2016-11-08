8a5993f35b08c889a05d42d6a456fd5b461b6747
bysen32 <bysen32@gmail.com>
2016-11-08 13:51:22 +0800

    Add New Test Project [FlappyBird]

---
+++ FlappyBird/Assets/Resources/Scripts/FlappyScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlappyScript : MonoBehaviour {
+
+    public float XSpeed = 1;
+    public float VelocityPreJump = 3;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+        if (GameStateManager.GameState == GameState.Intro)
+        {
+
+        }
+        else if (GameStateManager.GameState == GameState.Playing)
+        {
+            MoveBirdOnXAxis();
+            if (WasTouchedOrClicked())
+            {
+                BoostOnYAxis();
+            }
+
+        }
+        else if (GameStateManager.GameState == GameState.Dead)
+        {
+
+        }
+	}
+
+    void MoveBirdOnXAxis()
+    {
+        transform.position += new Vector3(Time.deltaTime * XSpeed, 0, 0);
+    }
+
+    bool WasTouchedOrClicked()
+    {
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended))
+            return true;
+        else
+            return false;
+    }
+
+    void BoostOnYAxis()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, VelocityPreJump);
+    }
+}
