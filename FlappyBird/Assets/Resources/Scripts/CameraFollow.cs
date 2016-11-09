c9fbed9c33aebe1dfe343cb3928c10b81ce1162f
bysen32 <bysen32@gmail.com>
2016-11-09 14:08:01 +0800

    Camera Follow Bird Move

---
+++ FlappyBird/Assets/Resources/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Player;
+    float cameraZ;
+    void Start()
+    {
+        cameraZ = transform.position.z;
+    }
+    void Update()
+    {
+        transform.position = new Vector3(Player.position.x, 0, cameraZ);
+    }
+}
