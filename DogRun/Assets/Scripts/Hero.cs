96b868f3237d83dcb3830211b914caaaf08949d0
bysen32 <bysen32@gmail.com>
2016-12-12 22:06:05 +0800

    WorkDay1. Search&GetResource && InitialMap && PlayerWalk+HandleMouseEvents

---
+++ DogRun/Assets/Scripts/Hero.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+    public float m_Velocity = 10;
+    public GameObject m_MoveObj;
+
+    void FixedUpdate()
+    {
+        if (m_MoveObj == null)
+            return;
+
+        float tm = Time.deltaTime;
+        Vector3 dir = new Vector3(0, 0, 1);
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.point.z > m_MoveObj.transform.position.z)
+                {
+                    m_MoveObj.transform.LookAt(new Vector3(hit.point.x, m_MoveObj.transform.position.y, hit.point.z));
+                }
+                Vector3 pos = transform.localPosition;
+                if (Mathf.Abs(hit.point.x - transform.position.x) > 2.0)
+                    dir += new Vector3(hit.point.x - transform.position.x, 0, 0).normalized;
+            }
+        }
+        else
+        {
+            m_MoveObj.transform.rotation = new Quaternion(0, 0, 0, 1);
+        }
+        transform.position += dir.normalized * m_Velocity * tm;
+    }
+}
---
+++ DogRun/Assets/Scripts/MainCameraScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraScript : MonoBehaviour {
+    public GameObject m_Target;
+    public float m_OffsetY;
+    public float m_OffsetZ;
+	void LateUpdate () {
+        if (m_Target == null)
+            return;
+        transform.position = m_Target.transform.position + new Vector3(0, m_OffsetY, m_OffsetZ);
+    }
+}
