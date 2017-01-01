44b7c7a2f7445af2c3fc9629e998149961a25bcb
bysen32 <bysen32@gmail.com>
2017-01-01 13:40:35 +0800

    Initiate Game【TapTap】

---
+++ TapTap/Assets/Scripts/UI/GameReady.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameReady : MonoBehaviour {
+    public GameObject m_GameStartBtn;
+    public GameObject m_GameExitBtn;
+
+	// Use this for initialization
+	void Start () {
+        UIEventListener.Get(m_GameStartBtn).onClick = OnGameStartBtnClick;
+        UIEventListener.Get(m_GameExitBtn).onClick = OnGameExitBtnClick;
+	}
+
+    private void OnGameStartBtnClick(GameObject btn) {
+        OpenUI("UI/GameScene");
+        Destroy(gameObject);
+    }
+    private void OnGameExitBtnClick(GameObject btn) {
+        Destroy(gameObject);
+    }
+
+    private void OpenUI(string path) {
+        GameObject ui = Instantiate(Resources.Load(path)) as GameObject;
+        ui.transform.parent = GameObject.Find("main").transform;
+        ui.transform.localPosition = Vector3.zero;
+        ui.transform.localScale = Vector3.one;
+    }
+}
---
+++ TapTap/Assets/Scripts/main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        OpenUI("UI/GameReady");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OpenUI(string path) {
+        GameObject ui = Instantiate(Resources.Load(path)) as GameObject;
+        ui.transform.parent = GameObject.Find("main").transform;
+        ui.transform.localPosition = Vector3.zero;
+        ui.transform.localScale = Vector3.one;
+    }
+}
